b8a544258fa5b2a10416cd008fec2e7dbb2a966a
beanxx <tnqls991211@naver.com>
2021-07-17 21:41:12 +0900

    feat: FriendshipTest Project Start

---
+++ src/Start.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Start = (props) => {
+  return (
+    <div className="container">
+      <div className="outter">
+        <h1>
+          나는 <span>{props.name}</span> 에 대해서
+          <br />
+          얼마나 알고 있을까?
+        </h1>
+        <input className="text-box" type="text" placeholder="내 이름" />
+        <button className="button">시작하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
